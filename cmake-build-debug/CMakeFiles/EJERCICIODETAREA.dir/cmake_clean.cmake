a0315f6c229152e51204298ec4c1fe7cfcfb23eb
Ana Junco <anajuncoescobar@gmail.com>
2021-08-02 23:00:31 -0500

    Vector

---
+++ cmake-build-debug/CMakeFiles/EJERCICIODETAREA.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EJERCICIODETAREA.dir/main.cpp.obj"
+  "EJERCICIODETAREA.exe"
+  "EJERCICIODETAREA.exe.manifest"
+  "EJERCICIODETAREA.pdb"
+  "libEJERCICIODETAREA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EJERCICIODETAREA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
